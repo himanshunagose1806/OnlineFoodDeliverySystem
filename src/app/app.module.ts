cd8ea437e19f2f85b3b8d0953e8f74cbea87e4af
Himanshu Nagose <himanshusanjiv.nagose@cognizant.com>
2025-09-02 09:30:20 +0530

    Components were added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { MenuComponent } from './menu/menu.component';
+import { OrderComponent } from './order/order.component';
+import { DeliveryComponent } from './delivery/delivery.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    MenuComponent,
+    OrderComponent,
+    DeliveryComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/delivery/delivery.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delivery',
+  standalone: false,
+  templateUrl: './delivery.component.html',
+  styleUrl: './delivery.component.css'
+})
+export class DeliveryComponent {
+
+}
