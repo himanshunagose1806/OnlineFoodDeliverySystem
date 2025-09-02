3512f16f851e27546780facee041cf12a80b9998
Himanshu Nagose <himanshusanjiv.nagose@cognizant.com>
2025-09-02 09:58:26 +0530

    Components were added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerComponent } from './customer/customer.component';
+import { MenuComponent } from './menu/menu.component';
+import { OrderComponent } from './order/order.component';
+import { DeliveryComponent } from './delivery/delivery.component';
+import { PaymentComponent } from './payment/payment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'customer', component:CustomerComponent},
+  {path : 'menu', component:MenuComponent},
+  {path : 'order', component:OrderComponent},
+  {path : 'delivery', component:DeliveryComponent},
+  {path : 'payment', component:PaymentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
